c992358ff95d7466b468202033c6bc528ced7409
austin metka <austin_metka@hotmail.com>
2018-03-02 17:13:06 +0000

    admin privledges now secure

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,14 +1,18 @@
 class Admin::CategoriesController < ApplicationController
+ http_basic_authenticate_with name: ENV['ADMIN_UN'], password: ENV['ADMIN_PW']
 
  def index
+  render
     @categories = Category.order(id: :desc).all
   end
 
   def new
+    render
     @category = Category.new
   end
 
   def create
+    render
     @category = Category.new(category_params)
 
     if @category.save
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_UN'], password: ENV['ADMIN_PW']
+
+
   def show
-  end
+     render
+   end
+
 end
